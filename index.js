c81985029a29078538ee6a721efb5d79858a9201
VikrantDhokale1 <139742612+VikrantDhokale1@users.noreply.github.com>
2023-08-29 18:24:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write('i m joseph kartik......'); //write a response to the client
   res.end(); //end the response
-}).listen(80); //the server object listens on port 4000
+}).listen(3000); //the server object listens on port 4000
